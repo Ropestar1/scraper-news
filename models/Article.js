fecd99d5f000e28399d93185687eaa511f87cb81
HQ <Spawn@calvisitor-10-105-169-181.calvisitor.1918.berkeley.edu>
2017-08-09 09:27:17 -0700

    Initializing files

---
+++ models/Article.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  // This only saves one note's ObjectId, ref refers to the Note model
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
