42231462aa18aaf5fc718e60a705f68c40098e07
HQ <Spawn@ROPERs-MacBook-Pro.local>
2017-08-10 22:51:00 -0700

    in progress

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-// CHANGE BELOW CODE WHEN DEPLOYING TO HEROKU
+// CHANGE BELOW CODE WHEN DEPLOYING TO HEROKU??????
 mongoose.connect("mongodb://localhost/scraping-mongoose");
 var db = mongoose.connection;
 
@@ -42,18 +42,42 @@ db.once("open", function() {
 });
 
 
+// Root get route
+app.get("/", function(req, res) {
+  var articleNoteArray = {
+    articles: [],
+    notes: []
+  };
+
+  Article.find({}, function(error, doc) {
+    if (error) console.log(error);
+
+    else {
+      for (var i =0; i < doc.length; i++) {
+        articleNoteArray.articles.push(doc[i]);
+      };
+      res.render('index', articleNoteArray);
+    }
+  });
+
+});
+
+app.get("/saved", function(req, res) {
+  res.render("saved"); // Add the handlebars object to this. 
+});
+
 // A GET request to scrape the echojs website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
   request("http://www.echojs.com/", function(error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
+
     var $ = cheerio.load(html);
     // Now, we grab every h2 within an article tag, and do the following:
     $("article h2").each(function(i, element) {
 
       // Save an empty result object
       var result = {};
-
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this).children("a").text();
       result.link = $(this).children("a").attr("href");
@@ -62,9 +86,10 @@ app.get("/scrape", function(req, res) {
       // This effectively passes the result object to the entry (and the title and link)
       var entry = new Article(result);
 
+      // articleArray.articles.push(entry);
+
       // Now, save that entry to the db
       entry.save(function(err, doc) {
-        // Log any errors
         if (err) {
           console.log(err);
         }
@@ -73,25 +98,8 @@ app.get("/scrape", function(req, res) {
           console.log(doc);
         }
       });
-
     });
-  });
-  // Tell the browser that we finished scraping the text
-  res.send("Scrape Complete");
-});
-
-// This will get the articles we scraped from the mongoDB
-app.get("/articles", function(req, res) {
-  // Grab every doc in the Articles array
-  Article.find({}, function(error, doc) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Or send the doc to the browser as a json object
-    else {
-      res.json(doc);
-    }
+    res.redirect('./')
   });
 });
 
